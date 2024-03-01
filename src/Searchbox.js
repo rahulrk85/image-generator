55c097eeaf870b24f17b8b40c25b6e8963e1f0d2
Rahul Kulkarni <rahulrkulkarni20@gmail.com>
2024-03-01 15:28:51 +0530

    rendering pages

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -12,19 +12,18 @@ const Searchbox = () => {
     e.preventDefault();
   }
 
-  click&&<div>
-    egrewjhrgtjhg
-  </div>
-
 
   return (
-    <div className=' flex justify-center mt-24'>
-        <form onSubmit={handleSubmit}>
-            <input className='border border-blue-900 p-2 m-2 px-48 pl-8 rounded-lg' type='text' placeholder='Search for an Image'/>
-            <button className='p-2 m-2 bg-blue-900 text-white rounded-lg'>Search</button>
-            <button className='p-2 px-3 m-2 bg-blue-900 text-white rounded-lg' onClick={handleClick}>Click for SUPRISE !</button>
-        </form>
-    </div>
+    !click?<div className=' flex justify-center mt-24'>
+    <form onSubmit={handleSubmit}>
+        <input className='border border-blue-900 p-2 m-2 px-48 pl-8 rounded-lg' type='text' placeholder='Search for an Image'/>
+        <button className='p-2 m-2 bg-blue-900 text-white rounded-lg'>Search</button>
+        <button className='p-2 px-3 m-2 bg-blue-900 text-white rounded-lg' onClick={handleClick}>Click for SUPRISE !</button>
+    </form>
+</div>:
+<div>
+  wefgwjeyfwyefgjwef
+</div>
   )
 }
 
