3b7645a222af08abdcc119f8c304d6229e786a00
Rahul Kulkarni <rahulrkulkarni20@gmail.com>
2024-03-01 14:40:19 +0530

    rendering pages

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Searchbox = () => {
+
+  const [click, setclick] = useState(false);
+
+  const handleClick=()=>{
+    setclick(true);
+    console.log(click);
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  }
+
+  click&&<div>
+    egrewjhrgtjhg
+  </div>
+
+
   return (
     <div className=' flex justify-center mt-24'>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input className='border border-blue-900 p-2 m-2 px-48 pl-8 rounded-lg' type='text' placeholder='Search for an Image'/>
             <button className='p-2 m-2 bg-blue-900 text-white rounded-lg'>Search</button>
+            <button className='p-2 px-3 m-2 bg-blue-900 text-white rounded-lg' onClick={handleClick}>Click for SUPRISE !</button>
         </form>
     </div>
   )
